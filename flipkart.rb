175ae7f27d00aa66245bfd43f22d24ac6efec9fb
vishalzambre <v.zambre@gmail.com>
2015-01-07 23:00:22 +0530

    modify url

--- flipkart.rb
+++ flipkart.rb
@@ -2,25 +2,26 @@ require 'rest-client'
 require './settings'
 
 class Flipkart
-	attr_accessor :affiliate_id, :token, :response_type
+	attr_accessor :affiliate_id, :token, :response_type, :url
 
-	def initialize(affiliate_id, token, response_type='json')
+	def initialize(affiliate_id, token, url, response_type='json')
 		@affiliate_id = affiliate_id
 		@token = token
+		@url = url
 		@response_type = response_type
 	end
 
 	def api_home			
-		return sendRequest(FLIPKART_BASE_API).body
+		return sendRequest.body
 	end
 
-	def call_url(url)
-		return sendRequest(url).body
+	def call_url
+		return sendRequest.body
 	end
 
 	private
-		def sendRequest(url)
+		def sendRequest
 			hearders = {'Cache-Control' => 'no-cache', 'Fk-Affiliate-Id' => @affiliate_id, 'Fk-Affiliate-Token' => @token }
-			RestClient.get url, hearders
+			RestClient.get @url, hearders
 		end	
 end
--- home.rb
+++ home.rb
@@ -8,16 +8,15 @@ require "base64"
 class Home < Sinatra::Base
 	Tilt.register Tilt::ERBTemplate, 'html.erb'
 	get '/' do
-		flipkart = Flipkart.new(FLIPKART_AFFILIATE_ID, FLIPKART_AFFILIATE_TOKEN)
+		flipkart = Flipkart.new(FLIPKART_AFFILIATE_ID, FLIPKART_AFFILIATE_TOKEN, FLIPKART_BASE_API)
 		flipkart = JSON.parse flipkart.api_home
 		@lists = flipkart['apiGroups']['affiliate']['apiListings']
   	erb :'index.html'
 	end
 
   get '/products' do
-		flipkart = Flipkart.new(FLIPKART_AFFILIATE_ID, FLIPKART_AFFILIATE_TOKEN)
-		url = Base64.decode64(params[:url])
-		details = JSON.parse flipkart.call_url(url)
+		flipkart = Flipkart.new(FLIPKART_AFFILIATE_ID, FLIPKART_AFFILIATE_TOKEN, Base64.decode64(params[:url]))
+		details = JSON.parse flipkart.call_url
 		@nextUrl = details['nextUrl']
 		@products = details['productInfoList']
 		puts @products
